a6eb1a424a6f4ec7605abcae158b08a841afa61a
Bulbul Arora <64860549+bulbularora@users.noreply.github.com>
2021-11-16 10:04:17 -0700

    Add files via upload

---
+++ Code/data.sql
@@ -0,0 +1,22 @@
+CREATE TABLE user(
+    user_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE events(
+    user_id INT,
+    event_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) not null,
+    course_name VARCHAR(30) not null,
+    type VARCHAR(30) not null,
+    due_date DATE,
+    due_time TIME,
+    description VARCHAR(300),
+    files VARCHAR(300),
+    PRIMARY KEY(event_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
