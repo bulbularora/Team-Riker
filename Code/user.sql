66b709b5027fd157acabb28e6806efd2efdc0e3b
archishab <78244432+archishab@users.noreply.github.com>
2021-11-29 16:23:33 -0600

    Update user.sql

--- Code/user.sql
+++ Code/user.sql
@@ -4,4 +4,4 @@ CREATE TABLE `User` (
  `email` varchar(30) NOT NULL,
  `password` varchar(10) NOT NULL,
  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4
