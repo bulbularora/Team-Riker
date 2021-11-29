1110152c8e72d94f66c9bfc7cb2a9f77a0bb912d
archishab <78244432+archishab@users.noreply.github.com>
2021-11-29 16:21:26 -0600

    Update data.sql

--- Code/data.sql
+++ Code/data.sql
@@ -1,22 +1,23 @@
-CREATE TABLE User(
-    user_id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    password VARCHAR(10) NOT NULL,
-    PRIMARY KEY (user_id)
-);
+CREATE TABLE `User` (
+ `user_id` int NOT NULL AUTO_INCREMENT,
+ `username` varchar(30) NOT NULL,
+ `email` varchar(30) NOT NULL,
+ `password` varchar(10) NOT NULL,
+ PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-CREATE TABLE Events(
-    user_id INT,
-    event_id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) not null,
-    course_name VARCHAR(30) not null,
-    type VARCHAR(30) not null,
-    due_date DATE,
-    due_time TIME,
-    description VARCHAR(500),
-    state VARCHAR(50),
-    PRIMARY KEY(event_id),
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
-);
+CREATE TABLE `Events` (
+ `user_id` int DEFAULT NULL,
+ `event_id` int NOT NULL AUTO_INCREMENT,
+ `title` varchar(30) NOT NULL,
+ `course_name` varchar(30) NOT NULL,
+ `type` varchar(30) NOT NULL,
+ `due_date` date DEFAULT NULL,
+ `due_time` time DEFAULT NULL,
+ `description` varchar(500) DEFAULT NULL,
+ `state` varchar(50) DEFAULT NULL,
+ PRIMARY KEY (`event_id`),
+ KEY `user_id` (`user_id`),
+ CONSTRAINT `Events_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4
 
