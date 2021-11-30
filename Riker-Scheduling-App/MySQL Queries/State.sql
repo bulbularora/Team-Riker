7e2ea1380c853b2c52ebb162b212b1f5dd43df5f
Bulbul Arora <64860549+bulbularora@users.noreply.github.com>
2021-11-30 14:31:18 -0700

    Update State.sql

--- Riker-Scheduling-App/MySQL Queries/State.sql
+++ Riker-Scheduling-App/MySQL Queries/State.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `State` (
+CREATE TABLE `state` (
  `state_id` int NOT NULL,
  `name` varchar(15) NOT NULL,
  `states` varchar(15) NOT NULL,
  PRIMARY KEY (`state_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+
