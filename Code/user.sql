3d11f088aee5f6cfba0d3045f17d7fa4af814eca
Archisha Bhattacharya <aba406@uregina.ca>
2021-11-30 18:12:18 -0600

    update repository

--- Code/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `User` (
- `user_id` int NOT NULL AUTO_INCREMENT,
- `username` varchar(30) NOT NULL,
- `email` varchar(30) NOT NULL,
- `password` varchar(10) NOT NULL,
- PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4
--- html/js/create-event-r.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("create-event-form").addEventListener("submit", CreateEventForm, false);
