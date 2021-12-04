9c3c0624453e66d55b7944aea30ede874645c191
archishab <78244432+archishab@users.noreply.github.com>
2021-12-04 17:45:29 -0600

    Update and rename Type.sql to type.sql

--- Riker-Scheduling-App/MySQL Queries/type.sql
+++ Riker-Scheduling-App/MySQL Queries/type.sql
@@ -4,3 +4,5 @@ CREATE TABLE `Type` (
  `types` varchar(15) NOT NULL,
  PRIMARY KEY (`type_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+
+INSERT INTO `Type` (`type_id`, `name`, `types`) VALUES ('1', 'Assignment', 'assignment'), ('2', 'Lab', 'lab'), ('3', 'Exam', 'exam') 
